620c53339c0f3a11274b3cde70ea1030dbcd99f3
oli1230 <obs1230@gmail.com>
2021-10-02 00:34:40 -0500

    working version for youtube

--- music_player.py
+++ music_player.py
@@ -7,41 +7,45 @@ class music_player(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def join(self, ctx):
-        if ctx.author.voice is None:
-            await ctx.send("You ain't in a voice channel you fuckin oaf")
-        voice_channel = ctx.author.voice.channel
-        if ctx.voice_client is None:
+    async def join(self, context):
+        if context.author.voice is None:
+            await context.send("You ain't in a voice channel you fuckin oaf")
+        voice_channel = context.author.voice.channel
+        if context.voice_client is None:
             await voice_channel.connect()
         else:
-            await ctx.voice_client.move_to(voice_channel)
+            await context.voice_client.move_to(voice_channel)
     
     @commands.command()
-    async def disconnect(self, ctx):
-        await ctx.voice_client.disconnect()
+    async def disconnect(self, context):
+        await context.voice_client.disconnect()
     
     @commands.command()
-    async def play(self, ctx, url):
-        ctx.voice_client.stop()
+    async def fuckoff(self, context):
+        await context.voice_client.disconnect()
+    
+    @commands.command()
+    async def play(self, context, url):
+        context.voice_client.stop()
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         YDL_OPTIONS = {'format': 'bestaudio'}
-        vc = ctx.voice_client
+        vc = context.voice_client
 
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
             url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(urls, **FFMPEG_OPTIONS)
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
             vc.play(source)
     
     @commands.command()
-    async def pause(self, ctx, url):
-        await ctx.voice_client.pause()
-        await ctx.send("You paused dat shit")
+    async def pause(self, context, url):
+        await context.voice_client.pause()
+        await context.send("You paused dat shit")
     
     @commands.command()
-    async def resume(self, ctx, url):
-        await ctx.voice_client.resume()
-        await ctx.send("You resumed dat shit")
+    async def resume(self, context, url):
+        await context.voice_client.resume()
+        await context.send("You resumed dat shit")
 
 def setup(client):
     client.add_cog(music_player(client))
